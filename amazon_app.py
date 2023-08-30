7565a67d1c3dcab40c7002c1801b53024bdb29a0
UMarehsa <umer.rana418@gmail.com>
2023-08-30 08:28:17 +0500

    Initial commit

---
+++ amazon_app.py
@@ -0,0 +1,61 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+class AmazonApp:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def allow_permissions(self):
+        wait = WebDriverWait(self.driver, 40)
+        wait.until(EC.presence_of_element_located((By.ID, 'com.android.permissioncontroller:id/permission_allow_button'))).click()
+
+    def skip_sign_in(self):
+        wait = WebDriverWait(self.driver, 30)
+        wait.until(EC.presence_of_element_located((By.ID, 'com.amazon.mShop.android.shopping:id/skip_sign_in_button'))).click()
+
+    def click_cart_icon(self):
+        time.sleep(5)
+        wait = WebDriverWait(self.driver, 20)
+        wait.until(EC.presence_of_element_located((By.XPATH, "//android.widget.FrameLayout[@content-desc='Cart 0 item Tab 3 of 4']/android.widget.ImageView"))).click()
+
+    def click_burger_menu(self):
+        wait = WebDriverWait(self.driver, 20)
+        burger_menu = wait.until(EC.presence_of_element_located((By.XPATH, '//android.widget.ImageView[@content-desc="Browse menu Tab 4 of 4"]')))
+        burger_menu.click()
+
+    def click_shop_by_department(self):
+        wait = WebDriverWait(self.driver, 20)
+        shop_by_department = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@text='Shop by Department']")))
+        shop_by_department.click()
+
+    def click_electronics_category(self):
+        wait = WebDriverWait(self.driver, 20)
+        electronics_category = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@text='Electronics']")))
+        electronics_category.click()
+
+    def click_home(self):
+        wait = WebDriverWait(self.driver, 20)
+        home_button = wait.until(EC.presence_of_element_located((By.XPATH, "//android.widget.ImageView[@content-desc='Home Tab 1 of 4']")))
+        home_button.click()
+
+    def click_deal_promotion(self):
+        wait = WebDriverWait(self.driver, 20)
+        deal_button = wait.until(EC.presence_of_element_located((By.XPATH, "//android.view.View[@content-desc='Shop deals ']/android.widget.TextView")))
+        deal_button.click()
+
+    def click_filters(self):
+        wait = WebDriverWait(self.driver, 20)
+        filter_button = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@text='Filters']")))
+        filter_button.click()
+
+    def click_see_more(self):
+        wait = WebDriverWait(self.driver, 20)
+        filter_button = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@text='See more']")))
+        filter_button.click()
+
+    def click_software(self):
+        wait = WebDriverWait(self.driver, 20)
+        filter_button = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@text='Software']")))
+        filter_button.click()
---
+++ amazon_test.py
@@ -0,0 +1,54 @@
+import time
+import unittest
+from appium import webdriver
+from amazon_app import AmazonApp
+import xmlrunner
+appium_server_url = 'http://localhost:4723'
+
+class TestAmazonApp(unittest.TestCase):
+    def setUp(self) -> None:
+        capabilities = dict(
+            platformName='Android',
+            automationName='uiautomator2',
+            deviceName='Android Emulator',
+            app=r"C:\Users\Lenovo\Downloads\amazon.apk",
+            appPackage="com.amazon.mShop.android.shopping",
+            appActivity="com.amazon.mShop.home.HomeActivity",
+            language='en',
+            locale='US'
+        )
+        self.driver = webdriver.Remote(appium_server_url, capabilities)
+        self.app = AmazonApp(self.driver)
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_total_number_for_category(self) -> None:
+        self.app.allow_permissions()
+        self.app.skip_sign_in()
+        time.sleep(5)
+        self.app.click_cart_icon()
+        self.app.click_home()
+        self.app.click_burger_menu()
+        self.app.click_shop_by_department()
+        time.sleep(5)
+        self.app.click_electronics_category()
+
+        # Wait for a moment to see the results
+        time.sleep(5)
+
+    def test_filter_by_department(self) -> None:
+        self.app.click_home()
+        time.sleep(5)
+        self.app.click_deal_promotion()
+        self.app.click_filters()
+        time.sleep(5)
+        self.app.click_see_more()
+        self.app.click_software()
+
+        # Wait for a moment to see the results
+        time.sleep(5)
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='report'))
